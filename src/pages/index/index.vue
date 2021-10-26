362d61a85cca3edf97c26f2c0a9bd255e1293c78
zhj1214 <zhj1214@hotmail.com>
2021-10-26 22:55:58 +0800

    feat: 暂停开发

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,45 @@
 <!--
- * @Description: 
+ * @Description: 首页
  * @Version: 0.0.1
  * @Autor: zhj1214
  * @Date: 2021-08-10 11:42:45
  * @LastEditors: zhj1214
- * @LastEditTime: 2021-09-13 21:19:42
+ * @LastEditTime: 2021-10-18 22:09:51
 -->
 <template>
   <view class="pageHome">
-    <navBar title="首页" background="#fff"></navBar>
-    你好，1111
+    <search class="search" v-if="isShowSearchView"> </search>
+    <view v-else class="">
+      <navBar title="吃啥" background="#fff"></navBar>
+      <view class="contentView">
+        <!-- 定位 -->
+        <view class="flex-center">
+          <img src="../../static/images/mapbz.png" style="width: 16px; height: 22px" />
+          <view class="adressText">{{ adress }}</view>
+          <img src="../../static/images/jtright.png" style="width: 20px; height: 18px" />
+        </view>
+        <!-- 搜索 -->
+        <view class="" style="margin: 10px 0" @click="showSearchView">
+          <u-search
+            placeholder="请输入商家、商品名称"
+            disabled
+            border-color="#ffce5d"
+            :show-action="false"
+          ></u-search>
+        </view>
+        <!-- 标题 -->
+        <view class="title"> 霸王餐 </view>
+        <!--筛选 -->
+        <view class="flex-between">
+          <view class="flex-center">
+            <view class=""> 距离排序 </view>
+            <view class=""> 佣金排序 </view>
+          </view>
+          <view class=""> 平台 </view>
+        </view>
+      </view>
+    </view>
+
     <!-- 提示信息弹框 -->
     <alertView
       v-if="alertShow && alertContent"
@@ -33,37 +63,33 @@
 
 <script>
   import alertView from '../../pageComponents/alertView/alertView'
-
-  const SCENE_PARSE_KEY = '$_scene_parsed' // 场景析构
+  import search from '../../pageComponents/search'
   export default {
     name: 'Home',
     components: {
       alertView,
+      search,
     },
-    /**
-     * 首页逻辑处理
-     * 1.是否需要选择商场
-     * 2.不同商场的场景模式，弹框切换商场
-     * 2.场景调用下一步
-     * 3.进入首页就获取用户信息，更新积分值
-     * */
-
     data() {
       return {
+        isShowSearchView: false,
         alertShow: false,
         mallAlertShow: false,
       }
     },
-    /**
-     * 生命周期函数--监听页面显示
-     * 没有options可以拿到
-     */
+    computed: {
+      adress() {
+        return '杭州市西湖区'
+      },
+    },
     onShow: function () {},
     onLoad(options) {
       uni.$eventbus.$on('testinitindexdata', (e) => {
         console.log(e, '11111111111111')
       })
       if (options) this.options = options
+
+      this.getLocation() // 获取定位信息
     },
     mounted() {
       console.log(this.$options.name, 'name')
@@ -96,7 +122,38 @@
       this.opacity = obj.scrollTop / 100
     },
 
-    methods: {},
+    methods: {
+      showSearchView() {
+        this.isShowSearchView = true
+      },
+      /**
+       * 获取经纬度
+       * */
+      getLocation() {
+        var self = this
+        uni.getLocation({
+          type: 'gcj02',
+          success(res) {
+            console.log(res)
+            self.getmallDatas({
+              longitude: res.longitude,
+              latitude: res.latitude,
+            })
+          },
+          fail(err) {
+            console.error(err)
+            uni.$alert.showToast('位置信息获取失败')
+          },
+        })
+      },
+      /**
+       * @description: 获取外卖列表数据
+       * @author: zhj1214
+       */
+      getmallDatas(val) {
+        console.log('开始获取活动数据', val)
+      },
+    },
     filters: {
       priceFormat(val) {
         if (!val) return '--'
@@ -106,8 +163,17 @@
   }
 </script>
 
-<style lang="less">
+<style lang="scss" scoped>
   .pageHome {
-    background-color: red;
+    .contentView {
+      padding: 32rpx;
+      .adressText {
+        @include text-style($size: 17px);
+        margin: 0px 4px;
+      }
+      .title {
+        @include text-style($size: 26px, $weight: 600);
+      }
+    }
   }
 </style>
--- src/pages/my/recharge.vue
+++ src/pages/my/recharge.vue
@@ -4,7 +4,7 @@
  * @Autor: zhj1214
  * @Date: 2021-09-17 22:24:59
  * @LastEditors: zhj1214
- * @LastEditTime: 2021-09-17 22:34:57
+ * @LastEditTime: 2021-10-18 20:54:16
 -->
 <template>
   <view class="view">
@@ -14,15 +14,39 @@
       <view class="title"> 充值金额 </view>
       <view class="flex-center">
         <view style="font-size: 26px"> ￥ </view>
-        <u-input></u-input>
+        <u-input v-model="price" type="number"></u-input>
       </view>
     </view>
+    <!-- 充值选项 -->
+    <view class="flex-center" style="justify-content: space-around; margin-top: 20px">
+      <view
+        class="pricebtn"
+        v-for="(item, index) in priceList"
+        :key="index"
+        @click="rechargePrice(item)"
+        >{{ item }}</view
+      >
+    </view>
+    <!-- 充值按钮 -->
+    <view class="oneRowBtn" style="margin-top: 20px">充值 </view>
   </view>
 </template>
 
 <script>
   export default {
     name: 'Recharge',
+    data() {
+      return {
+        price: 500,
+        priceList: [500, 1000, 1500, 2000],
+      }
+    },
+    methods: {
+      rechargePrice(val) {
+        this.price = val
+        console.log('老板充钱：', val)
+      },
+    },
   }
 </script>
 
@@ -38,6 +62,12 @@
         @include text-style($size: 18px, $weight: 400);
         margin-bottom: 20rpx;
       }
+    }
+    .pricebtn {
+      border: 1px solid #8b8b8b;
+      @include text-style($size: 16px);
+      padding: 6px 12px;
+      border-radius: 4px;
     }
   }
 </style>
